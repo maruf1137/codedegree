d4f326934853f9ad133c8bed3b45009c40f7dd20
maruf <mmaruf1137@gmail.com>
2022-04-30 17:16:58 +0600

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,21 @@
 $(".owl-carousel").owlCarousel({
   loop: true,
-  margin: 10,
+  margin: 30,
   nav: true,
   responsive: {
     0: {
       items: 1,
     },
-    400: {
+    750: {
       items: 2,
     },
-    600: {
-      items: 3,
-    },
-    1000: {
-      items: 5,
-    },
   },
+});
+
+const bars = document.querySelector(".bars");
+const sidebar = document.querySelector(".sidebar");
+
+bars.addEventListener("click", () => {
+  console.log("sidebar");
+  sidebar.classList.toggle("show-sidebar");
 });
