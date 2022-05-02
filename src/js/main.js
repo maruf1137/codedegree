ff9c2649fd32cdfc12d173b00048dd60c422bd05
maruf <mmaruf1137@gmail.com>
2022-05-02 15:10:46 +0600

    update

--- src/js/main.js
+++ src/js/main.js
@@ -24,3 +24,26 @@ closeSidebar.addEventListener("click", () => {
   console.log("sidebar");
   sidebar.classList.remove("show-sidebar");
 });
+
+// custom cursor
+
+const cursor = document.querySelector(".cursor");
+
+gsap.to(cursor, { xPercent: -50, yPercent: -50 });
+
+window.addEventListener("mousemove", (e) => {
+  console.log(e.x);
+  gsap.to(cursor, { x: e.x, y: e.y });
+});
+
+const cursorShow = () => {
+  cursor.style.display = "flex";
+};
+
+const cursorHide = () => {
+  cursor.style.display = "none";
+};
+
+const project = document.querySelector(".project");
+project.addEventListener("mouseover", cursorShow);
+project.addEventListener("mouseout", cursorHide);
