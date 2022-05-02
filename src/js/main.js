20ece05baa9c557e179500c58f95821a1d7e1943
maruf <mmaruf1137@gmail.com>
2022-05-02 23:15:43 +0600

    update

--- src/js/main.js
+++ src/js/main.js
@@ -37,11 +37,25 @@ project.addEventListener("mouseout", cursorHide);
 // swiper js
 
 var swiper = new Swiper(".projectSwiper", {
-  slidesPerView: 2,
+  slidesPerView: 1,
   spaceBetween: 30,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
   },
 });
 
