ad9ab20b644d9a4358f2a30594028ae4f677397d
maruf <mmaruf1137@gmail.com>
2022-05-02 23:33:16 +0600

    update

--- src/js/main.js
+++ src/js/main.js
@@ -44,17 +44,9 @@ var swiper = new Swiper(".projectSwiper", {
     clickable: true,
   },
   breakpoints: {
-    640: {
+    575: {
       slidesPerView: 2,
       spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-    1024: {
-      slidesPerView: 2,
-      spaceBetween: 30,
     },
   },
 });
